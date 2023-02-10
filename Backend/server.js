95ec9523271aa58b2839d8c2cb4321ce77c4d817
pritheesha-vai <pritheesha.rodrigues@vaimanagement.co>
2023-02-10 14:20:41 +0400

    2nd commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,8 @@
-console.log("hello world")
+const express = require('express')
+// const  dotenv = require('dotenv').config()
+// const port = process.dotenv.PORT
+const port= 5000;
+const app= express()
+
+app.listen(port, 'localhost', ()=> console.log(`server started on port number ${port}`) );
+
