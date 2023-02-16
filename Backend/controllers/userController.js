efa67a3b5ca926791470856d1a3a6e253431d200
pritheesha-vai <pritheesha.rodrigues@vaimanagement.co>
2023-02-16 12:39:07 +0400

    fetch user details

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -9,4 +9,35 @@ const fetchUser = expressAsyncHandler(async(req, res)=>{
     res.status(200).json({user});
 }
 )
-module.exports= fetchUser;
+
+
+const getUserByEmail = expressAsyncHandler(async(req, res)=>{
+
+
+   const email = req.body;
+   
+   const loggedInUser=await userModel.findOne(email);
+   
+   if(!loggedInUser)
+    {
+    res.status(400)
+    throw new Error('USer not found')
+    }
+    
+    res.json({
+
+    id: loggedInUser.id,
+    
+    email: loggedInUser.email,
+    
+    username : loggedInUser.username
+
+
+    })    // res.status(200).json({user});
+}
+)
+module.exports= {
+
+    fetchUser, getUserByEmail
+
+} 
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -4,9 +4,12 @@ const router = express.Router()
 
 const passport =require("passport")
 
-const  fetchUser = require('../controllers/userController');
+const  {fetchUser,getUserByEmail}  = require('../controllers/userController');
 
 
 router.route('/getUserDetails', passport.authenticate("jwt", {session:false, successFlash : "Sucessfuly authorized"}), {session:false}).get(fetchUser);
+
+router.route('/getUserbyEmail', passport.authenticate("jwt", {session:false, successFlash : "Sucessfuly authorized"}), {session:false}).get(getUserByEmail);
+
 
 module.exports=  router
